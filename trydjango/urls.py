58dd414af8a4cfcc025c0358d65faa1a21735af3
Saurabh Rawat <yores.2002@gmail.com>
2024-07-19 09:55:22 +0530

    recipe app search-updation-delete

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -18,10 +18,20 @@ from django.contrib import admin
 from django.urls import path
 from home.views import *
 from vege.views import *
-
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('',home,name="home"),
     path("abc/",success_page,name="success_page"),
     path("recipe/",recipe,name='recipe'),
+    path("delete_recipe/<id>/",delete_recipe,name="delete_recipe"),
+    path("update_recipe/<id>/",update_recipe,name="update_recipe"),
     path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL,
+                        document_root=settings.MEDIA_ROOT)
+
+urlpatterns+=staticfiles_urlpatterns()
--- vege/views.py
+++ vege/views.py
@@ -17,4 +17,33 @@ def recipe(request):
             recipe_description=recipe_descrip
         )
         return redirect("/recipe")
-    return render(request,"recipe.html")
+    queryset=Recipe.objects.all()
+
+    if request.GET.get('search'):
+        # yaha prr humne jo humare input field ka name tha waha se get krra
+        queryset=queryset.filter(recipe_name__icontains=request.GET.get('search'))
+        # __icontains m kya woh string aati h ya ni jitni bhij substring m woh search word hoga
+    context={"recipes":queryset}
+    return render(request,"recipe.html",context)
+
+def delete_recipe(request,id):
+    queryset=Recipe.objects.get(id=id)
+    queryset.delete()
+    return redirect("/recipe")
+
+def update_recipe(request,id):
+    queryset=Recipe.objects.get(id=id)
+    if request.method=="POST":
+        data=request.POST
+        recipe_name=data.get('recipe_name')
+        recipe_descrip=data.get('recipe_descrip')
+        recipe_image=request.FILES.get('recipe_image')
+        queryset.recipe_name=recipe_name
+        queryset.recipe_description=recipe_descrip
+        if recipe_image:
+            queryset.recipe_image=recipe_image
+        queryset.save()
+        return redirect("/recipe")
+    context={'recipes':queryset}
+    return render(request,"update_recipe.html",context)
+
